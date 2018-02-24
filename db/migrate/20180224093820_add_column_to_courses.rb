832f55f8147ab8b3c37e6151517aa716525f286b
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-02-24 15:09:28 +0530

    added fees to course

---
+++ db/migrate/20180224093820_add_column_to_courses.rb
@@ -0,0 +1,5 @@
+class AddColumnToCourses < ActiveRecord::Migration[5.1]
+  def change
+    add_column :courses, :fees, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224093154) do
+ActiveRecord::Schema.define(version: 20180224093820) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fees"
   end
 
 end
